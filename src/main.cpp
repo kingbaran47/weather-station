a6faa35839a05d363b0df0b3e1e6677ac8f5176d
Baran Evsen <baran47ps4@gmail.com>
2025-12-15 12:23:49 +0100

    Full project

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,104 @@
 #include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
+#include <WebServer.h>
+
+
+
+const char* WIFI_SSID = "YOUR_WIFI_NAME";
+const char *WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+
+//webserver details
+WebServer server(80);
+String header;
+unsigned long currentTime = millis();
+const long timeoutTime = 2000;
+unsigned long previousTime;
+
+//sensor details
+#define DHTPIN 18
+#define DHTTYPE DHT22
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+
+
+void handleRoot() {
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  float temp = event.temperature; //reads value from event
+  dht.humidity().getEvent(&event);
+  float hum = event.relative_humidity; //reads value from event
+
+  String html = "<!DOCTYPE html><html><body>";
+  html += "<h1>ESP32 Weatherstation</h1>";
+  html += "<p>Temperature: " + String(temp) + " °C</p>";
+  html += "<p>Humidity: " + String(hum) + " %</p>";
+  html += "</body></html>";
+
+  server.send(200, "text/html", html);
+}
+
+//Prepare response
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  //Connect to WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(400);
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected successfully.");
+  Serial.println("IP adress: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);
+  server.begin();
+  dht.begin();  
+  sensor_t sensor;
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
+
+  static unsigned long lastRead = 0;
+  if(millis() - lastRead > 2000) {
+lastRead = millis();
+  sensors_event_t event;
+  
+  dht.temperature().getEvent(&event);
+  if(isnan(event.temperature)) {
+    Serial.println("Error while reading temperature.");
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    Serial.print(" °C");
+    Serial.println();
+  }
+
+    dht.humidity().getEvent(&event);
+  if(isnan(event.relative_humidity)) {
+    Serial.println("Error while reading humidity.");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.print(" %");
+    Serial.println();
+  }
+
+  }
+  
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
